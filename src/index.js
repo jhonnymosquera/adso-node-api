f91d3b165fb43a7b1b7dc606e5103132d22a3a66
Jhonny Mosquera <78888142+jhonnymosquera@users.noreply.github.com>
2024-03-01 13:49:55 -0500

    testImagen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 
 const c = require('colors');
 const cors = require('cors');
@@ -9,6 +10,7 @@ const routes = require('./routes');
 // settings
 const app = express();
 const port = process.env.PORT || 9000;
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 // middlewares
 app.use(cors());
@@ -17,7 +19,11 @@ app.use('/api', routes);
 
 // route
 app.get('/', (req, res) => {
-	res.send('Welcome to my API');
+	res.sendFile(path.join(__dirname, 'src/public', 'index.html'));
+});
+
+app.get('/bot.qr.png', (req, res) => {
+	res.sendFile(path.join(__dirname, 'src/public', 'bot.qr.png'));
 });
 
 // mongodb connection
